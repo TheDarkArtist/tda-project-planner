e0091487c64a8dc46b831c1077eb0936449d419e
Kushagra <sparrow.kushagra@gmail.com>
2025-01-08 03:58:09 +0530

    feat(auth): add user button with sign out functionality

---
+++ src/features/auth/componenets/user-button.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useCurrentUser } from "../hooks/use-current-user";
+import { Loader, LogOutIcon } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
+
+function UserButton() {
+  const { data, isLoading } = useCurrentUser();
+  const { signOut } = useAuthActions();
+
+  if (isLoading) {
+    return <Loader className="size-4 text-slate-600 animate-spin" />;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  const avatarFallback = data.name!.charAt(0).toUpperCase();
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition">
+          <AvatarImage src={data.image} />
+          <AvatarFallback className="font-black text-xl text-slate-600">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="center"
+        side="right"
+        className="w-60"
+      >
+        <DropdownMenuItem onClick={() => signOut()}>
+          <LogOutIcon className="size-4" />
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default UserButton;
