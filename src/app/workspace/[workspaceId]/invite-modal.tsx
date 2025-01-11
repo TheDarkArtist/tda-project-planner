59d67a3e6051ded378b2978411ca92e99d287b41
Kushagra <sparrow.kushagra@gmail.com>
2025-01-11 06:48:18 +0530

    feat(invite modal): done

--- src/app/workspace/[workspaceId]/invite-modal.tsx
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -1,14 +1,17 @@
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { useNewJoinCode } from "@/features/workspaces/api/use-new-join-code";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { Copy } from "lucide-react";
+import { Copy, RefreshCcw } from "lucide-react";
 import { toast } from "sonner";
+import { useConfirm } from "@/hooks/use-confirm";
 
 interface InviteModalProps {
   open: boolean;
@@ -25,6 +28,33 @@ export const InviteModal = ({
 }: InviteModalProps) => {
   const workspaceId = useWorkspaceId();
 
+  const { mutate, isPending } = useNewJoinCode();
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "This will deactivate the current invite code and generate a new one.",
+  );
+
+  const handleNewCode = async () => {
+    const ok = await confirm();
+
+    if (!ok) return;
+
+    mutate(
+      {
+        workspaceId,
+      },
+      {
+        onSuccess: () => {
+          toast.success("Invite code regenerated");
+        },
+        onError: () => {
+          toast.error("Failed to regenerate invite code");
+        },
+      },
+    );
+  };
+
   const handleCopy = () => {
     const inviteLink = `${window.location.origin}/join/${workspaceId}`;
     navigator.clipboard.writeText(inviteLink).then(() => {
@@ -33,33 +63,49 @@ export const InviteModal = ({
   };
 
   return (
-    <Dialog
-      open={open}
-      onOpenChange={setOpen}
-    >
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Invite pepole to {name}</DialogTitle>
-          <DialogDescription>
-            Use the code below to invite pepole to your workspace, code is not
-            case sensitive
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col items-center justify-center gap-y-4 py-10">
-          <p className="text-4xl font-bold tracking-widest uppercase">
-            {joinCode}
-          </p>
-          <Button
-            className=""
-            variant="ghost"
-            size="sm"
-            onClick={handleCopy}
-          >
-            Copy link
-            <Copy className="size-4" />
-          </Button>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <>
+      <ConfirmDialog />
+      <Dialog
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite pepole to {name}</DialogTitle>
+            <DialogDescription>
+              Use the code below to invite pepole to your workspace, code is not
+              case sensitive
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col items-center justify-center gap-y-4 py-10">
+            <p className="text-4xl font-bold tracking-widest uppercase">
+              {joinCode}
+            </p>
+            <Button
+              className=""
+              variant="ghost"
+              size="sm"
+              onClick={handleCopy}
+            >
+              Copy link
+              <Copy className="size-4" />
+            </Button>
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <Button
+              variant="outline"
+              disabled={isPending}
+              onClick={handleNewCode}
+            >
+              New code
+              <RefreshCcw className="size-4" />
+            </Button>
+            <DialogClose asChild>
+              <Button>Close</Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
---
+++ src/features/workspaces/api/use-new-join-code.ts
@@ -0,0 +1,81 @@
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useCallback, useReducer, useMemo } from "react";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+type RequestType = { workspaceId: Id<"workspaces"> };
+type ResponseType = Id<"workspaces"> | null;
+
+type Options = {
+  onSuccess?: (data: ResponseType) => void;
+  onError?: (error: Error) => void;
+  onSettled?: () => void;
+  throwError?: boolean;
+};
+
+type State = {
+  data: ResponseType;
+  error: Error | null;
+  status: "success" | "error" | "settled" | "pending" | null;
+};
+
+type Action =
+  | { type: "pending" }
+  | { type: "success"; payload: ResponseType }
+  | { type: "error"; payload: Error }
+  | { type: "settled" };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "pending":
+      return { ...state, status: "pending", error: null, data: null };
+    case "success":
+      return { ...state, status: "success", data: action.payload, error: null };
+    case "error":
+      return { ...state, status: "error", error: action.payload };
+    case "settled":
+      return { ...state, status: "settled" };
+    default:
+      return state;
+  }
+};
+
+export const useNewJoinCode = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    data: null,
+    error: null,
+    status: null,
+  });
+
+  const mutation = useMutation(api.workspaces.newJoinCode);
+
+  const mutate = useCallback(
+    async (values: RequestType, options: Options = {}) => {
+      dispatch({ type: "pending" });
+
+      try {
+        const response = await mutation(values);
+        dispatch({ type: "success", payload: response });
+        options.onSuccess?.(response);
+        return response;
+      } catch (error) {
+        dispatch({ type: "error", payload: error as Error });
+        options.onError?.(error as Error);
+        if (options.throwError) {
+          throw error;
+        }
+      } finally {
+        dispatch({ type: "settled" });
+        options.onSettled?.();
+      }
+    },
+    [mutation],
+  );
+
+  const isPending = useMemo(() => state.status === "pending", [state.status]);
+  const isSuccess = useMemo(() => state.status === "success", [state.status]);
+  const isError = useMemo(() => state.status === "error", [state.status]);
+  const isSettled = useMemo(() => state.status === "settled", [state.status]);
+
+  return { mutate, ...state, isPending, isSuccess, isError, isSettled };
+};
