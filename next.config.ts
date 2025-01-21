57d3baff67e43bc4348ed264c5e7adb0ea270d18
Kushagra <sparrow.kushagra@gmail.com>
2025-01-22 05:02:22 +0530

    fix(messages/image): add new remote params to next config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "clean-bear-570.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "clean-bear-570.convex.site",
+      },
     ],
   },
 };
